01e2fa64b1a4202fb9627abb8bf290db8fb1559d
shivarajkulal <shivarajkulalsn884@gmail.com>
2024-02-29 19:07:21 +0530

    State in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import "./App.css";
-import Profile from "./components/Profile.js";
-import Welcome from "./components/Welcome.js";
-import Hello from "./components/Hello.js";
-import Greet from "./components/Greet.js";
+
+import Message from "./components/Message.js";
+
 function App() {
   return (
     <div className="App">
-      <Profile />
-      <Welcome />
-      <Hello />
-      <h1>############################</h1>
-      <Greet name="Amod" alias="Batman">
-        <p>random text Batman</p>
-      </Greet>
-      <Greet name="Shivaraj" alias="Superman">
-        <h2>
-          <p>random text Superman</p>
-          <button>Click here</button>
-        </h2>
-      </Greet>
+      <Message />
     </div>
   );
 }
